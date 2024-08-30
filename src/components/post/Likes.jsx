f0bb67111e0813054b4bc9a0953f56889d422eba
Matheus Martins Ciocca <143561783+MatheusCiocca@users.noreply.github.com>
2024-08-30 18:04:09 -0300

    Update Likes.jsx

--- src/components/post/Likes.jsx
+++ src/components/post/Likes.jsx
@@ -1,19 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { ref, get, update, onValue } from "firebase/database";
+import { ref, get, update } from "firebase/database";
 import { Button, Typography } from "@mui/material";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import { useAuth } from "../../context/AuthContext";
 import { database } from "../../service/firebase";
+import { getYouTubeID } from "../post/Post";
+import axios from 'axios';
 
 const Likes = React.memo(({ post, onLikeUpdate }) => {
     const [likes, setLikes] = useState(post.likes ? post.likes.length : 0);
+    const [likesYouTube, setLikesYouTube] = useState(0);
     const [likedPosts, setLikedPosts] = useState({});
     const [isUpdating, setIsUpdating] = useState(false);
     const { currentUser } = useAuth();
+    const API_KEY = import.meta.env.VITE_API_KEY;
 
     useEffect(() => {
         verificarLike(post.id);
     }, [post.id]);
+
+    useEffect(() => {
+        if (post.link) {
+            const videoId = getYouTubeID(post.link);
+            if (videoId) {
+                getLikesYouTubeCount(videoId).then(setLikesYouTube);
+            }
+        }
+    }, [post.link]);
+
+    const getLikesYouTubeCount = async (videoId) => {
+        try {
+          const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+            params: {
+              part: 'statistics',
+              id: videoId,
+              key: API_KEY,
+            },
+          });
+          const likeCount = response.data.items[0].statistics.likeCount;
+          return parseInt(likeCount, 10); // Certifique-se de retornar um número
+        } catch (error) {
+          console.error('Error fetching likes:', error);
+          return 0; // Retorne 0 em caso de erro
+        }
+      };
 
     const computarLike = async () => {
         if (isUpdating) return; // Evitar múltiplas atualizações simultâneas
@@ -88,7 +118,7 @@ const Likes = React.memo(({ post, onLikeUpdate }) => {
                     color: likedPosts[post.id] ? 'purple' : 'black',
                 }}
             />
-            <Typography style={{ color: 'black' }}> {likes} </Typography>
+            <Typography style={{ color: 'black' }}> {likes + likesYouTube} </Typography>
         </Button>
     );
 });
