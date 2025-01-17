9b6eb5e7a87f3e7b13c591b1d6988f962a8983f1
pampatec <codefolio@unipampa.edu.br>
2025-01-17 16:06:24 -0300

    feat:barra de progresso salva no bd e prevencao quanto a reload e fechamento da janela

--- src/components/post/components/postCard/VideoWatcher.jsx
+++ src/components/post/components/postCard/VideoWatcher.jsx
@@ -1,21 +1,30 @@
-// versão que conta o tempo assistido através do tempo atual do vídeo
-
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { ref, set, update, get, child } from 'firebase/database';
+import { database } from '../../../../service/firebase';
+import { useAuth } from '../../../../context/AuthContext';
 import { LinearProgress, Box, Typography } from '@mui/material';
 
-function VideoWatcher({ player }) {
+function VideoWatcher({ player, videoId, initialWatchTime, initialPercentageWatched }) {
     const [watchTime, setWatchTime] = useState(0);
     const [videoDuration, setVideoDuration] = useState(0);
-    const [percentageWatched, setPercentageWatched] = useState(0);
+    const [percentageWatched, setPercentageWatched] = useState(initialPercentageWatched || 0);
+    const [lastSavedPercentage, setLastSavedPercentage] = useState(initialPercentageWatched || 0);
+    const { currentUser } = useAuth();
+
+    useEffect(() => {
+        setPercentageWatched(initialPercentageWatched || 0);
+        setLastSavedPercentage(initialPercentageWatched || 0);
+        setWatchTime(initialWatchTime);
+    }, [initialPercentageWatched]);
 
     useEffect(() => {
         if (player) {
             setVideoDuration(player.getDuration());
 
             const interval = setInterval(() => {
-                if (player.getPlayerState() === 1) { // 1 = vídeo em reprodução
-                    const currentTime = player.getCurrentTime(); // Obtém o tempo atual do vídeo
+                if (player.getPlayerState() === 1) { // vídeo em reprodução
+                    const currentTime = player.getCurrentTime();
                     setWatchTime(currentTime); // Atualiza o tempo assistido com base no player
                 }
             }, 1000); // Verifica a cada segundo
@@ -26,10 +35,51 @@ function VideoWatcher({ player }) {
 
     useEffect(() => {
         if (videoDuration > 0) {
-            const percentage = (watchTime / videoDuration) * 100;
-            setPercentageWatched(percentage.toFixed(2)); // Define a porcentagem com precisão
+            const percentage = ((watchTime / videoDuration) * 100).toFixed(2);
+            setPercentageWatched(parseFloat(percentage)); // Define a porcentagem com precisão
+
+            if (parseFloat(percentage) - lastSavedPercentage >= 10) {
+                saveWatchTime(videoId, parseFloat(percentage));
+                setLastSavedPercentage(parseFloat(percentage) - (parseFloat(percentage) % 10)); // Atualiza a última porcentagem salva
+            }
         }
     }, [watchTime, videoDuration]);
+
+    useEffect(() => {
+        const handleBeforeUnload = (event) => {
+            event.preventDefault();
+            if (percentageWatched > lastSavedPercentage) {
+                saveWatchTime(videoId, percentageWatched);
+            }
+        };
+
+        window.addEventListener('beforeunload', handleBeforeUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, [percentageWatched, lastSavedPercentage, videoId]);
+
+    const saveWatchTime = async (videoId, percentage) => {
+        const watchTimeRef = ref(database, 'videoWatchTime');
+        const userVideoRef = child(watchTimeRef, `${currentUser.uid}_${videoId}`);
+    
+        const newWatchTimeData = {
+            videoId: videoId,
+            userId: currentUser.uid,
+            watchedTime: `${Math.floor(watchTime / 60)}min ${Math.round(watchTime % 60)}s`,
+            watchedTimeInSeconds: watchTime,
+            videoDuration: `${Math.floor(videoDuration / 60)}min ${Math.round(videoDuration % 60)}s`,
+            percentageWatched: percentage,
+        };
+    
+        const snapshot = await get(userVideoRef);
+        if (snapshot.exists()) {
+            await update(userVideoRef, newWatchTimeData);
+        } else {
+            await set(userVideoRef, newWatchTimeData);
+        }
+    };
 
     return (
         <div style={{ marginTop: '10px', textAlign: 'left' }}>
@@ -70,15 +120,17 @@ function VideoWatcher({ player }) {
 
 VideoWatcher.propTypes = {
     player: PropTypes.object,
+    videoId: PropTypes.string.isRequired,
+    initialWatchTime: PropTypes.number.isRequired,
+    initialPercentageWatched: PropTypes.number.isRequired,
 };
 
 export default VideoWatcher;
 
-
 // a ver:
 
-// captura dos eventos de fechar a página pra salvar o tempo assistido
+// captura dos eventos de fechar a página pra salvar o tempo assistido -> OK
 
-// pensar numa forma ok de salvar isso no banco de dados
+// pensar numa forma ok de salvar isso no banco de dados -> OK
 
 // tentar ver uma forma do player não poder ser avançado, apenas pausado e reproduzido -> não é uma grande prioridade
--- src/components/post/components/postCard/index.jsx
+++ src/components/post/components/postCard/index.jsx
@@ -1,138 +1,5 @@
-// import React, { useState, useEffect } from 'react';
-// import { Box, Card, CardContent, Typography, Grid, Divider, Avatar } from '@mui/material';
-// import PostMenu from '../menu/Menu';
-// import MembroLink from '../../../MembroLink';
-// import EditPostModal from '../editPost/EditPost';
-// import AddComment from '../addComment/AddComment';
-// import ShowComments from '../showComments/ShowComments';
-// // import Likes from '../../Likes';
-// import Likes from '../likes';
-// import YouTube from 'react-youtube';
-// import '../../post.css';
-// import { getYouTubeID } from '../../utils';
-// import Informacoes from '../informacoes/Informacoes';
-// import MyShare from '../myShare/MyShare';
-// import Tags from '../../PostTags';
-// import * as S from './styles';
-
-// export default function PostCards ({
-//     post,
-//     Edit,
-//     isEditModalOpen,
-//     setIsEditModalOpen,
-//     editingPost,
-//     Delete,
-//     comments,
-//     setComments,
-//     updateLikes,
-//     userRole,
-//     currentUser,
-//     onPostEdited
-// }) {
-//     const [isPostEdited, setIsPostEdited] = useState(false);
-//     const [showAddComment, setShowAddComment] = useState(false);
-
-//     useEffect(() => {
-//         if (isPostEdited) {
-//             onPostEdited();
-//             setIsPostEdited(false);
-//         }
-//     }, [isPostEdited, onPostEdited]);
-
-//     return(
-//         <S.Wrapper>
-//             <S.LineWrapper>
-//                 <S.ProfileButton>
-//                     <Avatar
-//                         src={post.userAvatar}
-//                         alt={post.user}
-//                         sx={{
-//                             width: 48,
-//                             height: 48
-//                         }}
-//                     />
-//                     <Typography component="div" variant="h6">
-//                         <MembroLink texto={post.user} user={post.uiUser} />
-//                     </Typography>
-//                 </S.ProfileButton>
-//                 <PostMenu post={post} onEdit={Edit} onDelete={Delete} />
-//             </S.LineWrapper>
-//             <S.LineWrapper style={{flexDirection: 'column'}}>
-//                 <Typography
-//                     component="div" 
-//                     sx={{
-//                         width: '100%',
-//                         fontSize: '15px',
-//                         marginBottom: '10px',
-//                         textAlign: 'left',
-//                         marginLeft: '3% !important',
-//                     }}
-//                 >
-//                     <b>{post.nome}</b>
-//                 </Typography>
-//                 <div style={{width: '100%'}}>
-//                 {post.link ? (
-//                         <>
-//                             <Divider sx={{
-//                                 displa: 'flex',
-//                                 justifyContent: 'center',
-//                                 alignSelf: 'center',
-//                                 width: '95%',
-//                                 borderBottom: '1px solid #ccc !important',
-//                                 marginTop: '10px !important',
-//                                 marginLeft: '6px',
-//                                 marginBottom: '10px !important',
-//                             }} />
-//                             <YouTube videoId={getYouTubeID(post.link)} opts={{ width: "95%", heigth: "95%" }} />
-//                             {/* <Divider className='divisor' /> */}
-//                             { /*<Tags tags={post.tags} />*/}
-//                             {/* <Divider className='divisor' /> */}
-//                             { /*<Comentarios postId={post.id} comments={comments} setComments={setComments} />*/}
-//                         </>
-//                     ) : (
-//                         <img
-//                             className="postImage"
-//                             src={post.userAvatar}
-//                             alt={post.user} />
-//                     )}
-//                 </div>
-//                 <div style={{width: '100%', marginLeft: '-30px'}}>
-//                     <Informacoes post={post} comments={comments} setComments={setComments} />
-//                     <Divider className='divisor' />
-//                 </div>
-//                 <S.LineWrapper style={{width: '98%', display: 'flex', justifyContent: 'space-around'}}>
-//                     <Likes post={post} onLikeUpdate={updateLikes} />
-//                     <ShowComments onShowComments={() => setShowAddComment(showAddComment ? false : true)} />
-//                     <MyShare post={post} />
-//                 </S.LineWrapper>
-//                 {showAddComment && (
-//                     <>
-//                         <Divider className='divisor' />
-//                         <AddComment postId={post.id} comments={comments} setComments={setComments} />
-//                     </>
-//                 )}
-//                 {(userRole === 'admin' || currentUser?.uid === post?.uidUser) && (
-//                     <>
-//                         {isEditModalOpen && (
-//                             <EditPostModal
-//                                 isOpen={isEditModalOpen}
-//                                 onClose={() => setIsEditModalOpen(false)}
-//                                 post={editingPost}
-//                                 onSave={() => {
-//                                     setIsPostEdited(true);
-//                                     setIsEditModalOpen(false);
-//                                 }}
-//                             />
-//                         )}
-//                     </>
-//                 )}
-//             </S.LineWrapper>
-//         </S.Wrapper>
-//     );
-// }
-
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, Divider, Avatar } from '@mui/material';
+import { Typography, Divider, Avatar } from '@mui/material';
 import PostMenu from '../menu/Menu';
 import MembroLink from '../../../MembroLink';
 import EditPostModal from '../editPost/EditPost';
@@ -144,6 +11,8 @@ import MyShare from '../myShare/MyShare';
 import { getYouTubeID } from '../../utils';
 import YouTube from 'react-youtube';
 import VideoWatcher from './VideoWatcher';
+import { ref, get, child } from 'firebase/database';
+import { database } from '../../../../service/firebase';
 import * as S from './styles';
 
 export default function PostCards ({
@@ -163,6 +32,8 @@ export default function PostCards ({
     const [isPostEdited, setIsPostEdited] = useState(false);
     const [showAddComment, setShowAddComment] = useState(false);
     const [playerInstance, setPlayerInstance] = useState(null);
+    const [initialWatchTime, setInitialWatchTime] = useState(0);
+    const [initialPercentageWatched, setInitialPercentageWatched] = useState(0);
 
     useEffect(() => {
         if (isPostEdited) {
@@ -171,8 +42,32 @@ export default function PostCards ({
         }
     }, [isPostEdited, onPostEdited]);
 
+    useEffect(() => {
+        const fetchWatchTime = async () => {
+            const watchTimeRef = ref(database, 'videoWatchTime');
+            const userVideoRef = child(watchTimeRef, `${currentUser.uid}_${post.id}`);
+            const snapshot = await get(userVideoRef);
+            if (snapshot.exists()) {
+                const data = snapshot.val();
+                setInitialWatchTime(data.watchedTimeInSeconds || 0);
+                setInitialPercentageWatched(parseFloat(data.percentageWatched) || 0);
+            }
+        };
+
+        if (currentUser && post.id) {
+            fetchWatchTime();
+        }
+    }, [currentUser, post.id]);
+
     const handlePlayerReady = (event) => {
         setPlayerInstance(event.target);
+    };
+
+    const handleStateChange = (event) => {
+        if (event.data === 1 && initialWatchTime > 0) { // vídeo em reprodução
+            event.target.seekTo(initialWatchTime);
+            setInitialWatchTime(0); // Reseta o tempo inicial
+        }  
     };
 
     return(
@@ -219,8 +114,8 @@ export default function PostCards ({
                                 marginLeft: '6px',
                                 marginBottom: '10px !important',
                             }} />
-                            <YouTube videoId={getYouTubeID(post.link)} opts={{ width: "95%", heigth: "95%" }} onReady={handlePlayerReady} />
-                            <VideoWatcher player={playerInstance} />
+                            <YouTube videoId={getYouTubeID(post.link)} opts={{ width: "95%", heigth: "95%" }} onReady={handlePlayerReady} onStateChange={handleStateChange} />
+                            <VideoWatcher player={playerInstance} videoId={post.id} initialWatchTime={initialWatchTime} initialPercentageWatched={initialPercentageWatched}/>
                         </>
                     ) : (
                         <img
