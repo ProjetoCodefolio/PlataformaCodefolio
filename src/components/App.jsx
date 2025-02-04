626d443476ec316c35f01c4ee4187286ae0857cc
eduardobulling <eduardobulling@gmail.com>
2025-02-04 18:50:28 -0300

    feat: add rout managedCourse

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,6 +25,7 @@ import HomePage from "../pages/homePage";
 import Cursos from "../pages/course/adminCourse";
 import ListCursos from "../pages/course/list";
 import Classes from "../pages/course/classes";
+import ManageMyCourses from "../pages/course/ManageMyCourses";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -126,6 +127,14 @@ function App() {
               element={
                 <PrivateRoute>
                   <Classes />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/manage-courses"
+              element={
+                <PrivateRoute>
+                  <ManageMyCourses />
                 </PrivateRoute>
               }
             />
